2db5fc6a560da6dd69730644176da8ce98a2e672
Sam Tran <samuelttran@gmail.com>
2023-11-01 15:44:44 -0600

    Write test for customer name, and add to view

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -21,19 +21,10 @@ RSpec.describe "Merchant Invoice Show", type: :feature do
       expect(page).to have_content("Created on: #{@invoice_1.formatted_date}")
     end
 
-    xit "They see customer first and last name" do
+    it "They see customer first and last name" do
       visit merchant_invoice_path(@merchant_1, @invoice_1)
 
-      expect(page).to have_content("Invoice ##{@invoice_1.id}")
+      expect(page).to have_content("Customer: #{@invoice_1.customer_full_name}")
     end
   end
 end
-
-# As a merchant
-# When I visit my merchant's invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
-# Then I see information related to that invoice including:
-
-# Invoice id
-# Invoice status
-# Invoice created_at date in the format "Monday, July 18, 2019"
-# Customer first and last name
