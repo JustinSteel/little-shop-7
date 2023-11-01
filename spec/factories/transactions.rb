b70d1ee8eb2b9f403bd09831e55e8827f5f5568d
Sam Tran <samuelttran@gmail.com>
2023-10-31 19:43:51 -0600

    Correct missing rand()

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :transaction do
     credit_card_number { Faker::Business.credit_card_number.delete('-') }
-    credit_card_expiration_date { "0#{rand(1..9)}/#{25..28}" }
+    credit_card_expiration_date { "0#{rand(1..9)}/#{rand(25..28)}" }
     result { rand(0..1)}
 
     association :invoice, factory: :invoice
