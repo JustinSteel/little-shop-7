32507ff42f81af45dd31a8611625a3d9fbb3ef34
Sam Tran <samuelttran@gmail.com>
2023-11-01 14:40:23 -0600

    Write tests for show page information

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -1,5 +1,39 @@
 require "rails_helper"
 
 RSpec.describe "Merchant Invoice Show", type: :feature do
+  describe "When a user visits merchant's invoice show, there is information" do
+    before(:each) do
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant_1)
+      @item_2 = create(:item, merchant: @merchant_1)
+      @customer_1 = create(:customer)
 
+      @invoice_1 = create(:invoice, customer: @customer_1)
+      create(:invoice_item, item: @item_1, invoice: @invoice_1)
+      # create(:invoice_item, item: @item_2, invoice: @invoice_1)
+    end
+
+    it "They see invoice id, status, create date in format 'Monday, July 18, 2019'" do
+      visit merchant_invoice_path(@merchant_1, @invoice_1)
+
+      expect(page).to have_content("Invoice ##{@invoice_1.id}")
+      expect(page).to have_content("Status: #{@invoice_1.status}")
+      expect(page).to have_content("Created on: #{@invoice_1.formatted_date}")
+    end
+
+    xit "They see customer first and last name" do
+      visit merchant_invoice_path(@merchant_1, @invoice_1)
+
+      expect(page).to have_content("Invoice ##{@invoice_1.id}")
+    end
+  end
 end
+
+# As a merchant
+# When I visit my merchant's invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
+# Then I see information related to that invoice including:
+
+# Invoice id
+# Invoice status
+# Invoice created_at date in the format "Monday, July 18, 2019"
+# Customer first and last name
