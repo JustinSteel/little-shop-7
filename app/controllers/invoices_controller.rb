d6f85b1fe699f4c78cae5789ca187ff940f7fb0b
Sam Tran <samuelttran@gmail.com>
2023-10-31 21:01:59 -0600

    Write test for class method invoices_for_merchant, add method to controller for instance variable, use instance variable @invoices in views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,5 +1,6 @@
 class InvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_merchant_id])
+    @invoices = Invoice.invoices_for_merchant(@merchant.id)
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe Invoice do
       )
     }
   end
+
+  describe 'class methods' do
+    before(:each) do
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant_1)
+      @customer_1 = create(:customer)
+
+      @invoice_1 = create(:invoice, customer: @customer_1)
+      create(:invoice_item, item: @item_1, invoice: @invoice_1)
+      @invoice_2 = create(:invoice, customer: @customer_1)
+      create(:invoice_item, item: @item_1, invoice: @invoice_2)
+      @invoice_3 = create(:invoice, customer: @customer_1)
+      create(:invoice_item, invoice: @invoice_3)
+    end
+
+    describe '.invoices_for_merchant' do
+      it 'returns list of invoices for a specific merchant id' do
+        expect(Invoice.invoices_for_merchant(@merchant_1.id)).to eq([@invoice_1, @invoice_2])
+      end
+    end
+  end
+
 end
