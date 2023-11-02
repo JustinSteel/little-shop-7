5cb8541ea8d105970c5f15f2dbf90afc10750fb4
Terry Lamb <terrylamb@gmail.com>
2023-11-02 17:43:44 -0400

    feat: finish US35. Still need to add model method test

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Admin Invoices Show Page" do
       expect(page).to have_content(@invoice_1a.id)
       within("#invoice-details") do
         expect(page).to have_content("Date Created: #{@invoice_1a.formatted_date}")
-        expect(page).to have_content("Customer Name: #{@invoice_1a.customer_name}")
+        expect(page).to have_content("Customer: #{@invoice_1a.customer_name}")
         expect(page).to have_content("Status: #{@invoice_1a.status}")
       end
     end
@@ -33,6 +33,16 @@ RSpec.describe "Admin Invoices Show Page" do
           
           expect(page).to_not have_content(@invoice_items15.item.name)
         end
+      end
+    end
+
+    # US 35
+    it "I see the total revenue that will be generated from this invoice" do
+
+      visit admin_invoice_path(@invoice_1a)
+
+      within("#total-revenue") do
+        expect(page).to have_content("Total Revenue: #{@invoice_1a.total_revenue}")
       end
     end
   end
