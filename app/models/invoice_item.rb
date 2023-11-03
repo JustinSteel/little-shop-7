530e5b844c649c8dd3ae0b6524d9bb5acd126580
Taylor Pubins <tpubz@icloud.com>
2023-11-03 16:07:23 -0700

    'Add test to ensure that  returns the correct list of invoice items ready to ship for a merchant on their dashboard.'

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,6 +10,5 @@ class InvoiceItem < ApplicationRecord
 
   def self.items_ready_to_ship
     where.not(status: "shipped")
-    # m.invoice_items.joins(:item).select("items.*, invoice_id").where.not(status: "shipped").group("items.id", :invoice_id).order("items.id")
   end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe InvoiceItem do
 
   describe "instance methods" do
     before(:each) do
-      load_test_data
+      # load_test_data
       @m = create(:merchant)
       @items = create_list(:item, 3, merchant: @m)
       @invoice_items = [
-        create(:invoice_item, item: @items[0], status: "packaged"),
+        create(:invoice_item, item: @items[0], status: "shipped"),
         create(:invoice_item, item: @items[0], status: "pending"),
         create(:invoice_item, item: @items[1], status: "packaged"),
         create(:invoice_item, item: @items[2], status: "packaged"),
@@ -30,9 +30,17 @@ RSpec.describe InvoiceItem do
     end
 
     describe "#items_ready_to_ship" do
-      it "returns list of items ready to ship when called on a merchant's invoice items" do
-        require 'pry'; binding.pry
-
+      it "returns list of invoice items ready to ship when called on a merchant's invoice items" do
+        # 4. Merchant Dashboard Items Ready to Ship
+        # As a merchant
+        # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+        # Then I see a section for "Items Ready to Ship"
+        # In that section I see a list of the names of all of my items that
+        # have been ordered and have not yet been shipped,
+        # And next to each Item I see the id of the invoice that ordered my item
+        # And each invoice id is a link to my merchant's invoice show page
+        # require 'pry'; binding.pry
+        expect(@m.invoice_items.items_ready_to_ship).to eq @invoice_items[1..3]
       end
     end
   end
